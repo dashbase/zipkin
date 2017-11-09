473b0831a9e343228e904fe264ce6dbda9dd44d8
RayeRen <3150103692@zju.edu.cn>
2017-11-09 13:35:35 +0800

    dashbase-zipkin hello world

--- zipkin-autoconfigure/storage-dashbase/src/main/java/zipkin/autoconfigure/storage/dashbase/ZipkinDashbaseStorageAutoConfiguration.java
+++ zipkin-autoconfigure/storage-dashbase/src/main/java/zipkin/autoconfigure/storage/dashbase/ZipkinDashbaseStorageAutoConfiguration.java
@@ -1,11 +1,15 @@
 package zipkin.autoconfigure.storage.dashbase;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import zipkin2.storage.StorageComponent;
+import zipkin.internal.V2StorageComponent;
+import zipkin.storage.StorageComponent;
+import zipkin2.dashbase.DashbaseStorage;
 
 @Configuration
 @EnableConfigurationProperties(ZipkinDashbaseStorageProperties.class)
@@ -14,4 +18,14 @@ import zipkin2.storage.StorageComponent;
 public class ZipkinDashbaseStorageAutoConfiguration {
   @Autowired(required = false)
   ZipkinDashbaseStorageProperties dashbase;
+
+  @Bean
+  StorageComponent storage(
+    @Value("${zipkin.storage.strict-trace-id:true}") boolean strictTraceId
+  ) {
+    return V2StorageComponent.create(DashbaseStorage.newBuilder()
+      .strictTraceId(strictTraceId)
+      .build());
+  }
+
 }
--- zipkin-storage/dashbase/src/main/java/zipkin2/dashbase/DashbaseStorage.java
+++ zipkin-storage/dashbase/src/main/java/zipkin2/dashbase/DashbaseStorage.java
@@ -1,40 +1,48 @@
 package zipkin2.dashbase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import zipkin2.storage.SpanConsumer;
 import zipkin2.storage.SpanStore;
 import zipkin2.storage.StorageComponent;
 
 public class DashbaseStorage extends StorageComponent {
+  private static final Logger logger = LoggerFactory.getLogger(DashbaseStorage.class);
 
-    public static Builder newBuilder() {
-        return new Builder();
-    }
+  public static Builder newBuilder() {
+    return new Builder();
+  }
 
-    public static final class Builder extends StorageComponent.Builder {
-        boolean strictTraceId = true;
-        /** {@inheritDoc} */
-        @Override public Builder strictTraceId(boolean strictTraceId) {
-            this.strictTraceId = strictTraceId;
-            return this;
-        }
+  public static final class Builder extends StorageComponent.Builder {
+    boolean strictTraceId = true;
 
-        @Override public DashbaseStorage build() {
-            return new DashbaseStorage(this);
-        }
-    }
-
-    DashbaseStorage(Builder builder) {
-
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Builder strictTraceId(boolean strictTraceId) {
+      this.strictTraceId = strictTraceId;
+      return this;
     }
 
     @Override
-    public SpanStore spanStore() {
-        return new DashbaseSpanStore();
+    public DashbaseStorage build() {
+      return new DashbaseStorage(this);
     }
+  }
 
-    @Override
-    public SpanConsumer spanConsumer() {
-        // return new DashbaseSpanConsumer();
-        return null;
-    }
+  DashbaseStorage(Builder builder) {
+    logger.warn("Hello world!");
+  }
+
+  @Override
+  public SpanStore spanStore() {
+    return new DashbaseSpanStore();
+  }
+
+  @Override
+  public SpanConsumer spanConsumer() {
+    // return new DashbaseSpanConsumer();
+    return null;
+  }
 }
